fbcda89733ac250dbd96d81bc177f5e966c081ab
Yuji Nomura <ugnom1121@gmail.com>
2019-07-25 22:43:16 +0900

    abc110-d

--- abc/abc110/d.py
+++ abc/abc110/d.py
@@ -1,2 +1,28 @@
-words = lambda t : list(map(t, input().split()))
-s = input()
+if __name__ == '__main__':
+    words = lambda t : list(map(t, input().split()))
+    n,m = words(int)
+
+import math
+def primes(x):
+    if x < 2:
+        return []
+    ps = [i for i in range(x)]
+    ps[1] = 0
+    for p in ps:
+        if p > math.sqrt(x): break
+        if p == 0: continue
+        for np in range(2 * p, x, p):
+            ps[np] = 0
+    return [p for p in ps if p != 0]
+
+
+def prime_factors(ps, x):
+    pfs = []
+    for p in ps:
+        i = 0
+        while x % p == 0:
+            x = x // p
+            i += 1
+        if i != 0:
+            pfs.append((p,i))
+    return pfs
--- mymath.py
+++ mymath.py
@@ -7,3 +7,28 @@ def gcd(x,y):
 
 def lcm(x,y):
     return (x * y) // gcd(x,y)
+
+import math
+def primes(x):
+    if x < 2:
+        return []
+    ps = [i for i in range(x)]
+    ps[1] = 0
+    for p in ps:
+        if p > math.sqrt(x): break
+        if p == 0: continue
+        for np in range(2 * p, x, p):
+            ps[np] = 0
+    return [p for p in ps if p != 0]
+
+
+def prime_factors(ps, x):
+    pfs = []
+    for p in ps:
+        i = 0
+        while x % p == 0:
+            x = x // p
+            i += 1
+        if i != 0:
+            pfs.append((p,i))
+    return pfs
