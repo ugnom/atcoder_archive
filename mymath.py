6e3a323ae533b9fc41d4a18307f0d2648cbb71af
Yuji Nomura <ugnom1121@gmail.com>
2019-07-26 23:59:44 +0900

    math

--- mymath.py
+++ mymath.py
@@ -32,3 +32,23 @@ def prime_factors(ps, x):
         if i != 0:
             pfs.append((p,i))
     return pfs
+
+def mod_comb(a,b,mod):
+    #print(a,b)
+    if a-b < b: return mod_comb(a,a-b,mod)
+    ans_mul = 1
+    ans_div = 1
+    for i in range(b):
+        ans_mul *= a-i
+        ans_div *= i+1
+        ans_mul %= mod
+        ans_div %= mod
+    return (ans_mul * modpow(ans_div, mod-2, mod)) % mod
+
+def modpow(a,b,mod):
+    if b == 0: return 1
+    if b % 2 == 0:
+        h = modpow(a, b//2, mod)
+        return (h * h) % mod
+    else:
+        return (a * modpow(a, b-1, mod)) % mod
