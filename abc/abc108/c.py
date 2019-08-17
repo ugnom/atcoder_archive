0839d699da2cecdee904b8e734df240e50622c35
Yuji Nomura <ugnom1121@gmail.com>
2019-08-17 17:23:17 +0900

    abc108

--- abc/abc108/c.py
+++ abc/abc108/c.py
@@ -1,5 +1,7 @@
 words = lambda t : list(map(t, input().split()))
 n,k = words(int)
 
-for i in range(1,n+1):
-    
+if k % 2 == 1:
+    print((n // k)**3)
+else:
+    print((n//k)**3 + (n//(k//2)-(n//k))**3)
--- abc/abc108/d.py
+++ abc/abc108/d.py
@@ -1,3 +1,11 @@
 words = lambda t : list(map(t, input().split()))
-n = input()
-xs = words(int)
+l = int(input())
+
+import math
+p = math.log(l,2)
+r = l - 2**p
+
+ans = []
+for i in p:
+    ans.append((i+1,i+2,0))
+    ans.append((i+1,i+2,1))
