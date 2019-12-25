6b0925a7d673b35fd1e5dc919daa9ee7c1d36f45
Yuji Nomura <ugnom1121@gmail.com>
2019-12-25 19:57:48 +0900

    templ

--- templ.rs
+++ templ.rs
@@ -1,13 +1,17 @@
 use std::str::FromStr;
 
-fn getOne<T : FromStr>() -> T {
+fn get_one<T : FromStr>() -> T {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).ok();
     return s.trim().parse().ok().unwrap();
 }
 
-fn getVec<T : FromStr>() -> Vec<T> {
+fn get_vec<T : FromStr>() -> Vec<T> {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).ok();
     return s.trim().split_whitespace().map(|e| e.parse().ok().unwrap()).collect();
+}
+
+fn main() {
+
 }
